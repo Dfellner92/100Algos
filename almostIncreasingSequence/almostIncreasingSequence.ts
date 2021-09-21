e63c33b06daa7dd0ded00da76d868afb1f4b2a8c
Dan Fellner <dfellner92@gmail.com>
2021-09-21 16:37:53 -0400

    almost inc

---
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -0,0 +1,14 @@
+export function almostIncreasingSequence(sequence: number[]): boolean {
+    let count = 0;
+    for (let i = 0; i < sequence.length; i++) {
+      if (count === 2) {
+        return false;
+      } else if (sequence[i] > sequence[i + 1]) {
+        count++;
+      }
+    }
+    return true;
+  }
+  
+  console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+  console.log(almostIncreasingSequence([1, 3, 2]))  
