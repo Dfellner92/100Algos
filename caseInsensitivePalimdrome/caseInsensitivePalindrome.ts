90acf47ced692b42bb13eac9d8b4617e06754fcb
Dan Fellner <dfellner92@gmail.com>
2021-10-08 17:17:05 -0400

    century from year

---
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -0,0 +1,8 @@
+function isCaseInsensitivePalindrome(inputString: string): boolean {
+  const originalLowerCase = inputString.toLowerCase();
+  const reversedString = originalLowerCase.split("").reverse().join("");
+  return originalLowerCase === reversedString;
+}
+
+console.log(isCaseInsensitivePalindrome("AaBaa"));
+console.log(isCaseInsensitivePalindrome("abac"));
---
+++ centuryFromYear/centuryFromYear.ts
@@ -0,0 +1,6 @@
+function centuryFromYear(year: number): number {
+  return year % 100 === 0 ? Math.floor(year / 100) : Math.floor(year / 100 + 1);
+}
+
+console.log(centuryFromYear(1905));
+console.log(centuryFromYear(1700));
