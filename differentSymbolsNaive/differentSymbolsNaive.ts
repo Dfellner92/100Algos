ce9a8bce99ca8cc0ca136bb236f971bdf1499aaf
Dan Fellner <dfellner92@gmail.com>
2021-10-10 14:05:50 -0400

    different symbols naive

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,12 @@
 export function differentSymbolsNaive(s: string): number {
-
+  let container: string[] = [];
+  let splitString = s.split("");
+  for (const character of splitString) {
+    if (!container.includes(character)) {
+      container.push(character);
+    }
+  }
+  return container.length;
 }
 
-// console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive("cabca"));
