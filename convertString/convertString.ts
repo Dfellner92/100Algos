db259cc08e4454f4cdd7413913842f4458429444
Dan Fellner <dfellner92@gmail.com>
2021-10-09 20:28:59 -0400

    convert C to F, convert String

---
+++ convertString/convertString.ts
@@ -0,0 +1,18 @@
+export function convertString(s: string, t: string): boolean {
+  let splitS: string[] = s.split("");
+  let splitT: stirng[] = t.split("");
+  let index = 0;
+  let hiddenWord = "";
+
+  for (let i = 0; i < splitS.length; i++) {
+    if (splitS[i] === splitT[index]) {
+      hiddenWord = hiddenWord.concat(splitS[i]);
+      index++;
+    }
+  }
+
+  return hiddenWord === t;
+}
+
+console.log(convertString("ceoydefthf5iyg5h5yts", "codefights"));
+console.log(convertString("addbyca", "abcd"));
