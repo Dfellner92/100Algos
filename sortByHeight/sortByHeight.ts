0593d974bf9c41d5ec652689238ee2894ed32ab7
Dan Fellner <dfellner92@gmail.com>
2021-11-21 17:26:48 -0500

    sort by height

--- sortByHeight/sortByHeight.ts
+++ sortByHeight/sortByHeight.ts
@@ -1,5 +1,19 @@
 export function sortByHeight(a: number[]): number[] {
-return [];
+  let treeArray: number[] = [];
+  let newArr = [];
+
+  a.forEach((height) => height > -1 && treeArray.push(height));
+  treeArray.sort();
+
+  a.forEach((height) => {
+    if (height > -1) {
+      newArr.push(treeArray[0]);
+      treeArray.shift();
+    } else {
+      newArr.push(-1);
+    }
+  });
+  return newArr;
 }
 
-// console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
