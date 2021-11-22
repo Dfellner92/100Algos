ca26197d64058b4e5628bfc846f3fb2afab62d19
Dan Fellner <dfellner92@gmail.com>
2021-11-21 20:51:45 -0500

    stringsConsruction

--- stringsConstruction/stringsConstruction.ts
+++ stringsConstruction/stringsConstruction.ts
@@ -1,5 +1,19 @@
-export function stringsConstruction(a: string, b: string): number {
+export function stringsConstruction(a: string, b: string): any {
+  let stringMap: object = {};
+  let propertyArray: number[] = [];
 
+  for (let i = 0; i < a.length; i++) {
+    stringMap[a[i]] = 0;
+  }
+  for (let j = 0; j < b.length; j++) {
+    if (stringMap.hasOwnProperty(b[j])) {
+      stringMap[b[j]] = stringMap[b[j]] + 1;
+    }
+  }
+  for (const property in stringMap) {
+    propertyArray.push(stringMap[property]);
+  }
+  propertyArray.sort();
+  return propertyArray[0];
 }
-
-// console.log(stringsConstruction('abc', 'abccba'));
+console.log(stringsConstruction("abc", "abccba"));
