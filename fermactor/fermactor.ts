f6b021d665548143e368350d2b2cce1ccc925d2c
Dan Fellner <dfellner92@gmail.com>
2021-11-23 07:46:00 -0500

    fermactor

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,12 @@
 export function fermactor(n: number): number[] {
-
+  let index = 1;
+  while (index < n) {
+    let x = n + Math.pow(index, 2);
+    if (Number.isInteger(Math.sqrt(x))) {
+      return [Math.sqrt(x), index];
+    }
+    index++;
+  }
 }
 
-// console.log(fermactor(15));
+console.log(fermactor(15));
