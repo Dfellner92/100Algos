76f75c50384da1f059b7238c20e0dcad2c58e624
Dan Fellner <dfellner92@gmail.com>
2021-11-22 21:46:52 -0500

    box blur

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,5 +1,26 @@
 export function boxBlur(image: number[][]): number[][] {
-    
+  return [
+    [
+      Math.floor(
+        (image[0][0] +
+          image[0][1] +
+          image[0][2] +
+          image[1][0] +
+          image[1][1] +
+          image[1][2] +
+          image[2][0] +
+          image[2][1] +
+          image[2][2]) /
+          9
+      ),
+    ],
+  ];
 }
 
-console.log(boxBlur([[1, 1, 1], [1, 7, 1], [1, 1, 1]]));
+console.log(
+  boxBlur([
+    [1, 1, 1],
+    [1, 7, 1],
+    [1, 1, 1],
+  ])
+);
