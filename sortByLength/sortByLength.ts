c3506f21bd24dce316efa6fa2559e7ced825ef44
Dan Fellner <dfellner92@gmail.com>
2021-11-24 17:36:47 -0500

    sort by length

---
+++ sortByLength/sortByLength.ts
@@ -0,0 +1,7 @@
+export function sortByLength(inputArray: string[]): string[] {
+  return inputArray.sort((a, b) => {
+    return a.length - b.length;
+  });
+}
+
+console.log(sortByLength(["abc", "", "aaa", "a", "zz"]));
