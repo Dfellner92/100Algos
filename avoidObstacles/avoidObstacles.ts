44b2275b52681395651e4f3fed77fb9d7e4022f5
Dan Fellner <dfellner92@gmail.com>
2021-11-27 22:54:54 -0500

    avoid obstacles

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,15 @@
 export function avoidObstacles(inputArray: number[]): number {
+  let inputSort: number[] = inputArray.sort((a, b) => a - b);
+  let index: number = 1;
 
+  while (index < inputSort[inputSort.length - 1]) {
+    let pass = inputSort.every((num) => num % index !== 0);
+    if (pass === true) {
+      return index;
+    } else {
+      index++;
+    }
+  }
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([5, 3, 6, 7, 9]));
