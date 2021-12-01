ffb360c5b59c2b8e4da158c34a7630ad5a1cb47a
Dan Fellner <dfellner92@gmail.com>
2021-12-01 18:09:09 -0500

    crossing Sum

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,18 @@
 export function crossingSum(matrix: number[][], a: number, b: number): number {
-
+  const rowTotal = matrix[a].reduce((a, b) => a + b);
+  let columnTotal = 0;
+  matrix.forEach((arr, i) => (columnTotal += arr[b]));
+  return rowTotal + columnTotal - matrix[a][b];
 }
 
-// console.log(crossingSum([[1, 1, 1, 1], 
-//     [2, 2, 2, 2], 
-//     [3, 3, 3, 3]], 1, 3));
+console.log(
+  crossingSum(
+    [
+      [1, 1, 1, 1],
+      [2, 2, 2, 2],
+      [3, 3, 3, 3],
+    ],
+    1,
+    3
+  )
+);
