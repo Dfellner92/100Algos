e33eccd096cca9a8545138e15b90616a2df82887
Dan Fellner <dfellner92@gmail.com>
2021-12-02 21:40:32 -0500

    sumAllPrimes

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,6 +1,18 @@
 export function sumAllPrimes(num: number): number {
-
+  let index = 0;
+  for (let i = 1; i <= num; i++) {
+    let ind = 0;
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) {
+        ind++;
+      }
+    }
+    if (ind === 2) {
+      index += i;
+    }
+  }
+  return index;
 }
 
-// console.log(sumAllPrimes(10));
-// console.log(sumAllPrimes(977));
+console.log(sumAllPrimes(10));
+console.log(sumAllPrimes(977));
