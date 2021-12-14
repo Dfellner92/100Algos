0d6a0466f49b34fcdcb409d4452094e8228fab7c
Dan Fellner <dfellner92@gmail.com>
2021-12-13 21:36:35 -0500

    incremental backups

---
+++ incrementalBackups/incrementalBackups.ts
@@ -0,0 +1,19 @@
+export function incrementalBackups(
+  lastBackupTime: number,
+  changes: number[][]
+): number[] {
+  const backupIDs: number[] = [];
+  const arr = changes.filter((change) => change[0] > lastBackupTime);
+  arr.forEach(
+    (change) => !backupIDs.includes(change[1]) && backupIDs.push(change[1])
+  );
+  return backupIDs;
+}
+
+// console.log(incrementalBackups(461620205, [[461620203, 1],
+//     [461620204, 2],
+//     [461620205, 6],
+//     [461620206, 5],
+//     [461620207, 3],
+//     [461620207, 5],
+//     [461620208, 1]]));
